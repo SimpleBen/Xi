a0d3aaba0127ad3e4f4193735ef337aa9eea8409
simpleben <417131610@qq.com>
2018-10-24 23:41:10 +0800

    fixed tencent & index

--- assets/js/project.js
+++ assets/js/project.js
@@ -1,12 +1,13 @@
 $(function(){
     var win_w = $(window).width();
+    var current_top = 0;
     var nav_num = $('.menu-ul > li').not('.font_padding').length;
     var toggleSelect = function(w){
         if(w<=240){$(".sub").removeClass("select");}
-        for(var i = 1;i<= nav_num;i++){
-            if(($(" .b"+i).offset().top - 50)<w){
+        for(var i = 0; i < nav_num; i++){
+            if(($(".bT").eq(i).offset().top - 50)<w){
                 $(".sub").removeClass("select");
-                $(".p"+i).addClass("select");
+                $(".pT").eq(i).addClass("select");
             }
         }
     }
@@ -27,10 +28,8 @@ $(function(){
         if(win_w>415){
             if ($('.project-narrow-content').offset().top - whs < 0){
                 $(".project-aside").addClass('project-aside-float');
-                $(".float-nav").addClass('float-nav-active')
             } else {
                 $(".project-aside").removeClass('project-aside-float');
-                $(".float-nav").removeClass('float-nav-active')
             }
             // if($('.top-nav').css('opacity')>0 || whs < 200){
             //     $('.top-nav').css('opacity',(60-whs)/60);
@@ -38,6 +37,18 @@ $(function(){
             // }else{$('.top-nav').hide();}
         }
         toggleSelect(whs);
+
+        if( whs >= current_top ){ //向下滑动
+            if (!$('.float-nav').hasClass('float-nav-hide')) {
+                $('.float-nav').addClass('float-nav-hide');
+            }
+            current_top = whs;
+        }else{
+            if ($('.float-nav').hasClass('float-nav-hide')) {
+                $('.float-nav').removeClass('float-nav-hide');
+            }
+            current_top = whs;
+        }
     });
     $('.project-nav-toggle').on('click',function(e){
         $('.project-nav-toggle').toggleClass('project-nav-toggle-n');
